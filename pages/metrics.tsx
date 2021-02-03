c544452ddd77b3d0f40c7e963b8f900ca0d34f95
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-02-03 20:54:46 +0200

    Improve page sizing

--- pages/metrics.tsx
+++ pages/metrics.tsx
@@ -12,7 +12,7 @@ const Metrics: FC = () => {
       </Head>
 
       <div className="flex items-center justify-center min-h-screen bg-gray-200">
-        <div className="w-4/5">
+        <div className="w-4/5 md:w-3/5">
           <ClusterMetrics />
         </div>
       </div>
